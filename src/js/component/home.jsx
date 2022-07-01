e55a8dd7427b42b43f2b63506771433ea28e0136
IsaGalo80 <isagalo1980@outlook.com>
2022-07-01 11:57:09 +0000

    falta terminar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,6 +7,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [listTask, setListTask] = useState([]);
+  
+useEffect(
+  ()=>{
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/IsaG', {
+      method: "GET",
+    })
+    .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+        console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(data => {
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        setListTask(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+    })
+    .catch(error => {
+        //manejo de errores
+        console.log(error);
+    })
+  },[]
+)
+
+
   const deletTask = (posicion) => {
     console.log("ejecutado");
     let newList = listTask.filter((value, index) => posicion !== index);
@@ -14,46 +38,46 @@ const Home = () => {
   };
   return (
     <>
-      <div id="box">
+    <body>
         <div id="box1">
-          <div id="tex1">
-            <h1>Todo List!!</h1>
-            <input
+            <h1>todos</h1>
+              <div className="PB">
+            <input id="tarea"
               onChange={(event) => {
                 setInputValue(event.target.value);
               }}
             ></input>
-            <button
-              id="click"
+            <button className="btn btn-default"
+              // id="click"
               onClick={() => {
                 setListTask([...listTask, inputValue]);
               }}
             >
-              GUARDAR
+              Add
             </button>
-          </div>
+            </div>
           <div>
             <ul>
-              {listTask.map((value, index) => {
+  {listTask.map((value, index) => {
                 return (
-                  <li key={index}>
+                  <div id="listas" key={index}>
                     {value}{" "}
-                    <span
+                    <span id="bCerrar"class="btn-close" aria-label="Close"
                       onClick={() => {
                         deletTask(index);
                       }}
                     >
-                      borrar
                     </span>
-                  </li>
+                  </div>
                 );
               })}
-            </ul>
+</ul>
           </div>
         </div>
-      </div>
+      </body>
     </>
   );
 };
-
+   
+     
 export default Home;
