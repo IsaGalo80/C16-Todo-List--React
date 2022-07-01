08e16bd0c9a5fa491c8b1acfef0c7d472e150f7f
IsaGalo80 <isagalo1980@outlook.com>
2022-07-01 16:19:54 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,29 +7,26 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [listTask, setListTask] = useState([]);
-  
-useEffect(
-  ()=>{
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/IsaG', {
+
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/IsaG", {
       method: "GET",
     })
-    .then(resp => {
+      .then((resp) => {
         console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
         console.log(resp.status); // el código de estado = 200 o código = 400 etc.
         console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
         return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
+      })
+      .then((data) => {
         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
         setListTask(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-    })
-    .catch(error => {
+      })
+      .catch((error) => {
         //manejo de errores
         console.log(error);
-    })
-  },[]
-)
-
+      });
+  }, []);
 
   const deletTask = (posicion) => {
     console.log("ejecutado");
@@ -38,16 +35,18 @@ useEffect(
   };
   return (
     <>
-    <body>
+      <body>
         <div id="box1">
-            <h1>todos</h1>
-              <div className="PB">
-            <input id="tarea"
+          <h1>todos</h1>
+          <div className="PB">
+            <input
+              id="tarea"
               onChange={(event) => {
                 setInputValue(event.target.value);
               }}
             ></input>
-            <button className="btn btn-default"
+            <button
+              className="btn btn-default"
               // id="click"
               onClick={() => {
                 setListTask([...listTask, inputValue]);
@@ -55,29 +54,30 @@ useEffect(
             >
               Add
             </button>
-            </div>
+          </div>
           <div>
             <ul>
-  {listTask.map((value, index) => {
+              {listTask.map((value, index) => {
                 return (
                   <div id="listas" key={index}>
                     {value}{" "}
-                    <span id="bCerrar"class="btn-close" aria-label="Close"
+                    <span
+                      id="bCerrar"
                       onClick={() => {
                         deletTask(index);
                       }}
                     >
+                      <i className="fa-solid fa fa-trash"></i>
                     </span>
+                    <hr />
                   </div>
                 );
               })}
-</ul>
+            </ul>
           </div>
         </div>
       </body>
     </>
   );
 };
-   
-     
 export default Home;
