398fc5cc1c917c5f23fb05d8ea3ec9b64f62b6bf
IsaGalo80 <isagalo1980@outlook.com>
2022-07-03 11:33:12 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,22 +11,53 @@ const Home = () => {
   useEffect(() => {
     fetch("https://assets.breatheco.de/apis/fake/todos/user/IsaG", {
       method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
       .then((resp) => {
         console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-        console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+        // console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+        // console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
         return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
       })
       .then((data) => {
         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        setListTask(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        setListTask(data.map((tarea) => tarea.label)); //esto imprimirá en la consola el objeto exacto recibido del servidor
       })
       .catch((error) => {
         //manejo de errores
         console.log(error);
       });
   }, []);
+
+  useEffect(() => {
+    setTimeout(() =>
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/IsaG", {
+        method: "PUT",
+        body: JSON.stringify(
+          listTask.map((tarea) => ({ label: tarea, done: false }))
+        ),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => {
+          console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+          console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+          console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+          return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+        })
+        .then((data) => {
+          //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+          console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        })
+        .catch((error) => {
+          //manejo de errores
+          console.log(error);
+        })
+    );
+  }, 10000);
 
   const deletTask = (posicion) => {
     console.log("ejecutado");
@@ -38,42 +69,37 @@ const Home = () => {
       <body>
         <div id="box1">
           <h1>todos</h1>
-          <div className="PB">
+          <div className="box2">
             <input
               id="tarea"
               onChange={(event) => {
                 setInputValue(event.target.value);
               }}
-            ></input>
-            <button
-              className="btn btn-default"
-              // id="click"
-              onClick={() => {
-                setListTask([...listTask, inputValue]);
-              }}
+              value={inputValue}
+              onKeyDown={(e) => {
+                if(e.key==="Enter"){
+                  setListTask([...listTask, inputValue]);
+                  setInputValue("")}
+                }}
             >
-              Add
-            </button>
-          </div>
-          <div>
-            <ul>
-              {listTask.map((value, index) => {
-                return (
-                  <div id="listas" key={index}>
-                    {value}{" "}
-                    <span
-                      id="bCerrar"
-                      onClick={() => {
-                        deletTask(index);
-                      }}
-                    >
-                      <i className="fa-solid fa fa-trash"></i>
-                    </span>
-                    <hr />
-                  </div>
-                );
-              })}
-            </ul>
+            </input>
+            <div>
+              <ul>
+                {listTask.map((value, index) => {
+                  return (
+                    <div id="listas" key={index}>
+                      {value}{" "}
+                      <span
+                        onClick={() => {
+                          deletTask(index);
+                        }}
+                      ><i id="bCerrar" className="fa-solid fa fa-trash"></i>
+                      </span>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
       </body>
